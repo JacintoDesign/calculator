12b15a979ead503e11eb4052d1bfa09c03353547
JACINTO WONG <jacinto.wong@cbc.ca>
2020-06-24 11:02:05 -0400

    js: calculation functionality

--- script.js
+++ script.js
@@ -3,9 +3,19 @@ const inputBtns = document.querySelectorAll('button');
 const clearBtn = document.getElementById('clear-btn');
 const equalBtn = document.getElementById('equal-btn');
 
-// function updateDisplay() {
+let firstValue = 0;
+let operatorValue = '';
+let awaitingSecondValue = false;
 
-// }
+function sendInputValue(value) {
+  if (awaitingSecondValue) {
+    calculatorDisplay.textContent = value;
+    awaitingSecondValue = false;
+  } else {
+    const displayValue = calculatorDisplay.textContent;
+    calculatorDisplay.textContent = displayValue === '0' ? value : displayValue + value;
+  }
+}
 
 function addDecimal() {
   if (!calculatorDisplay.textContent.includes('.')) {
@@ -13,28 +23,50 @@ function addDecimal() {
   }
 }
 
-function inputValue(value) {
-  // console.log(value);
-  const displayValue = calculatorDisplay.textContent;
-  calculatorDisplay.textContent = displayValue === '0' ? value : displayValue + value;
+const calculate = {
+  '/': (firstNumber, secondNumber) => firstNumber / secondNumber,
+
+  '*': (firstNumber, secondNumber) => firstNumber * secondNumber,
+
+  '+': (firstNumber, secondNumber) => firstNumber + secondNumber,
+
+  '-': (firstNumber, secondNumber) => firstNumber - secondNumber,
+
+  '=': (firstNumber, secondNumber) => secondNumber,
+};
+
+function useOperator(operator) {
+  const currentValue = parseFloat(calculatorDisplay.textContent);
+  if (!firstValue) {
+    firstValue = currentValue;
+  } else if (operatorValue) {
+    const calculation = calculate[operatorValue](firstValue, currentValue);
+    console.log('calculation', calculation);
+    calculatorDisplay.textContent = calculation;
+    firstValue = calculation;
+  }
+  awaitingSecondValue = true;
+  operatorValue = operator;
+  console.log('firstValue', firstValue);
+  console.log(operatorValue);
+
 }
+
+
 
 inputBtns.forEach((inputBtn) => {
   if (inputBtn.classList.length === 0) {
-    inputBtn.addEventListener('click', () => inputValue(inputBtn.value));
+    inputBtn.addEventListener('click', () => sendInputValue(inputBtn.value));
   } else if (inputBtn.classList.contains('operator')) {
-    inputBtn.addEventListener('click', () => inputValue(inputBtn.value));
+    inputBtn.addEventListener('click', () => useOperator(inputBtn.value));
   } else if (inputBtn.classList.contains('decimal')) {
     inputBtn.addEventListener('click', () => addDecimal());
   }
 });
 
 function resetDisplay() {
+  firstValue = 0;
   calculatorDisplay.textContent = '0';
-}
-
-function calculate() {
-
 }
 
 // Event Listeners
